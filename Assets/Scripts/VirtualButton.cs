a726d23ac23a6d1126e63b91bb29679d78f3c727
DESKTOP-Q5M49A3\paulb <paulbelches@gmail.com>
2018-05-09 08:56:53 -0600

    Entrega final
    Funcionamiento completado con los 3 pajaros

---
+++ Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+/*Paul Belches 17088
+ Rodrigo Morales 17027
+ Laboratorio 6
+ 9/05/18*/
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler 
+{
+    private VirtualButtonBehaviour virtualBtn;
+    public Animator birdAnim;
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //Metodo para poner el sonido
+        birdAnim.SetTrigger("sing");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+       
+    }
+
+    // Use this for initialization
+    void Start () {
+        //Metodo para obtener el boton
+        virtualBtn = GetComponent<VirtualButtonBehaviour>();
+        virtualBtn.RegisterEventHandler(this);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
